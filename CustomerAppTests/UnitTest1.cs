c12dbd7a2213f5257fa72e9430e3323893fa86c9
cwighty <agentcboy@gmail.com>
2023-03-24 08:47:13 -0600

    set up tests

---
+++ CustomerAppTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CustomerAppTests;
+
+public class CacheServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
