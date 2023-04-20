10066fab99b4f5a26b4472824a9767f66afad3b7
cwighty <agentcboy@gmail.com>
2023-04-20 17:21:15 -0600

    login/logout store app

--- StoreApp/Services/AutorizationService.cs
+++ StoreApp/Services/AutorizationService.cs
@@ -1,5 +1,43 @@
-﻿namespace StoreApp.Services;
+﻿using Microsoft.AspNetCore.Components;
+using Supabase;
+
+namespace StoreApp.Services;
 
 public class AuthorizationService
 {
+    private readonly Client client;
+
+    public AuthorizationService(Client client)
+    {
+        this.client = client;
+    }
+
+    public async Task LogIn(string email, string password)
+    {
+        await client.Auth.SignInWithPassword(email, password);
+    }
+
+    public async Task Register(string email, string password)
+    {
+        var session = await client.Auth.SignUp(email, password);
+    }
+
+    public async Task Logout()
+    {
+        await client.Auth.SignOut();
+    }
+
+    public bool IsLoggedIn()
+    {
+        return client.Auth.CurrentSession != null;
+    }
+
+    public bool IsAdmin()
+    {
+        if (IsLoggedIn())
+        {
+            return client.Auth.CurrentUser?.Email.Contains("admin@thesodageyser.com") ?? false;
+        }
+        return false;
+    }
 }
