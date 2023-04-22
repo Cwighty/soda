3e21c812f8b208c918fdf4eb963b79af1242d348
andrewbengordon <andrewbengordon@gmail.com>
2023-04-22 14:44:15 -0600

    Fixed notifications and aesthetic fixes

--- supabase/scripts/Schema.sql
+++ supabase/scripts/Schema.sql
@@ -222,3 +222,6 @@ $$ language plpgsql security definer;
 create trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
+
+
+alter publication supabase_realtime add table purchase;
