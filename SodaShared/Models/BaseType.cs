73844c3d13498f134bfa9ee73d559000030d6da5
andrewbengordon <andrewbengordon@gmail.com>
2023-04-27 17:51:35 -0600

    Grouped products by base in store app

--- SodaShared/Models/BaseType.cs
+++ SodaShared/Models/BaseType.cs
@@ -5,6 +5,18 @@ public class BaseType
     public int Id { get; set; }
     public string Name { get; set; }
     public List<Size> Sizes { get; set; } = new();
+    public override bool Equals(object? obj)
+    {
+        if (obj is BaseType other)
+        {
+            return Id == other.Id;
+        }
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return Id;
+    }
 }
 
 public static class BaseTypeExtensions
