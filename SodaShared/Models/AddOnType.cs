4929be15435aaedef90d52d39a6cd27938a40a4c
andrewbengordon <andrewbengordon@gmail.com>
2023-04-27 18:14:02 -0600

    Add On types categories ya know what I'm sayin boiiiiiiiii

--- SodaShared/Models/AddOnType.cs
+++ SodaShared/Models/AddOnType.cs
@@ -5,6 +5,18 @@ public class AddOnType
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public override bool Equals(object? obj)
+    {
+        if (obj is AddOnType other)
+        {
+            return Id == other.Id;
+        }
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 }
 
 public static class AddOnTypeExtensions
